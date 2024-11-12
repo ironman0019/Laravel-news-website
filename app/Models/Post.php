e71465c9f2fb87b8423c49fcd8af60b64f71b87e
Emad Isavi <emadesavi@gmail.com>
2024-11-13 01:11:39 +0330

    Add relation with user & category

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Post extends Model
 {
     use SoftDeletes;
+
+    // Relation with user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Relation with category
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
