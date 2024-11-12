fb69487091db3214c3b6e11703c000d5532f24d0
Emad Isavi <emadesavi@gmail.com>
2024-11-13 01:28:18 +0330

    Add create function

--- app/Http/Controllers/admin/PostController.php
+++ app/Http/Controllers/admin/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -22,7 +23,8 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::all();
+        return view('admin.post.create', compact('categories'));
     }
 
     /**
