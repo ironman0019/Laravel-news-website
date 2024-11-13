339db8a8ba6f71ddd76a4a653fec94a0323a2585
Emad Isavi <emadesavi@gmail.com>
2024-11-13 18:40:29 +0330

    Use paginate for posts index

--- app/Http/Controllers/admin/PostController.php
+++ app/Http/Controllers/admin/PostController.php
@@ -18,7 +18,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::paginate(4);
         return view('admin.post.index', compact('posts'));
     }
 
