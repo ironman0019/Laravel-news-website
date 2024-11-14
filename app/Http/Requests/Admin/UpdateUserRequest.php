1229e2210483c4bb29ae17366a7e48c48fb7e499
Emad Isavi <emadesavi@gmail.com>
2024-11-14 04:33:00 +0330

    Create admin update user request and add validation rules

---
+++ app/Http/Requests/Admin/UpdateUserRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|max:120|min:2|regex:/^[ا-یa-zA-Z0-9\-۰-۹ء-ي., ]+$/u',
+            'email' => 'required|email|unique:users,email',
+            'status' => 'required|numeric|digits_between:0,1'
+        ];
+    }
+}
