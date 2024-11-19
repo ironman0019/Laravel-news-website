0f47755069c799b8215163b5b9a1624afb4e0dc7
Emad Isavi <emadesavi@gmail.com>
2024-11-20 03:15:55 +0330

    Create home controller and return index page view

---
+++ app/Http/Controllers/Home/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view('app.index');
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function category()
+    {
+
+    }
+}
