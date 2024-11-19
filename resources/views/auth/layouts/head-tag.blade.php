e33b0e28964561ef3e5884305e039c8d82e33c6a
Emad Isavi <emadesavi@gmail.com>
2024-11-19 03:41:09 +0330

    Add auth layouts

---
+++ resources/views/auth/layouts/head-tag.blade.php
@@ -0,0 +1,7 @@
+    <title>@yield('title', 'عنوان')</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="{{ asset('auth/assets/images/icons/favicon.ico') }}" />
+    <!--===============================================================================================-->
+    @include('auth.layouts.styles')
---
+++ resources/views/auth/layouts/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('auth.layouts.head-tag')
+</head>
+
+<body>
+
+    @yield('content')
+
+
+
+    @include('auth.layouts.scripts')
+
+</body>
+
+</html>
