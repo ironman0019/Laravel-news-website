45f56eda0b15a2f22f691353c7077461356cf739
Emad Isavi <emadesavi@gmail.com>
2024-12-04 22:38:00 +0330

    Add selected posts and most commented posts to all view pages

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Menu;
+use App\Models\Post;
 use App\Models\WebSetting;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
@@ -29,5 +30,11 @@ class AppServiceProvider extends ServiceProvider
 
         $menus = Menu::where('parent_id', null)->get();
         view()->share('menus', $menus);
+
+        $selectedPosts = Post::latest()->where('selected', 1)->limit(3)->get();
+        view()->share('selectedPosts', $selectedPosts);
+
+        $mostCommentedPosts = Post::withCount('comments')->orderBy('comments_count', 'desc')->limit(5)->get();
+        view()->share('mostCommentedPosts', $mostCommentedPosts);
     }
 }
